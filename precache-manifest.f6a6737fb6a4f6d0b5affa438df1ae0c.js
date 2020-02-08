517573f26af07cfad82ca8cf6fedf5f9d7eaf19a
dejunnn <e0324575@u.nus.edu>
2020-02-08 15:12:32 +0800

    Updates

---
+++ precache-manifest.f6a6737fb6a4f6d0b5affa438df1ae0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1c8517f03b69ba284578448f1858df",
+    "url": "/React-P1/index.html"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/React-P1/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cb558964815f9582466a",
+    "url": "/React-P1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "190c41214d85c83a9834",
+    "url": "/React-P1/static/js/2.d2203df8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-P1/static/js/2.d2203df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb558964815f9582466a",
+    "url": "/React-P1/static/js/main.60575ff6.chunk.js"
+  },
+  {
+    "revision": "a246db2f8028eaa3df71",
+    "url": "/React-P1/static/js/runtime-main.ee9dbc46.js"
+  }
+]);
